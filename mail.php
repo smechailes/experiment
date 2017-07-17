57ac40918f2693282a18d8d101d59e1311034131
smechailes <smechailes@gmail.com>
2017-07-17 17:10:23 +0545

    mail was sent and database was created for the first time

---
+++ mail.php
@@ -0,0 +1,14 @@
+<?php include "head.php";?>
+<?php
+$from= "smechailes@gmail.com";
+$Headers = "";
+$header .= "MIME-Version: 1.0 \r\n";
+$header .= "Content-type: text/html; Charset= iso-859-1 \r\n";
+$header .= "From: ".$from." \r\n";
+$to = "setok321@gmail.com";
+$subject = "test-mail";
+$message ="<h1>hello</h1>";
+$mail = mail($to, $subject, $message, $header);
+echo (int)$mail;
+?>
+<?php include "foot.php";?>
