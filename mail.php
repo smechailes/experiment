7c1c18ab49b3f7f27628556b589fca5528eb1a93
smechailes <smechailes@gmail.com>
2017-07-18 16:41:09 +0545

    sql query were created

--- mail.php
+++ mail.php
@@ -1,7 +1,7 @@
 <?php include "head.php";?>
 <?php
 $from= "smechailes@gmail.com";
-$Headers = "";
+$header = "";
 $header .= "MIME-Version: 1.0 \r\n";
 $header .= "Content-type: text/html; Charset= iso-859-1 \r\n";
 $header .= "From: ".$from." \r\n";
