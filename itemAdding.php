557fdc242a8fb45fefd22995b3f94f776595c2a5
smechailes <smechailes@gmail.com>
2017-07-19 17:32:02 +0545

    new files for item adding form was created

---
+++ itemAdding.php
@@ -0,0 +1,32 @@
+<html>
+<head><title>form</title>
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"/>
+<script
+  src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+  integrity="sha256-k2WSCIexGzOj3Euiig+TlR8gA0EmPjuc79OEeY5L45g="
+  crossorigin="anonymous"></script>
+</head>
+<body>
+<h1><center>Items Add</center></h1>
+
+<div class="container">
+<div class="form-group row">
+  <label for="example-text-input" class="col-md-2 col-form-label">Item</label>
+  <div class="col-md-10">
+    <input class="form-control" type="text" value="Lenovo vibe p1m" id="example-text-input" name="item">
+  </div>
+</div>
+<div class="form-group row">
+  <label for="example-search-input" class="col-md-2 col-form-label">Description</label>
+  <div class="col-md-10">
+    <input class="form-control" type="search" value="Smartphone" id="example-search-input">
+  </div>
+</div>
+
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+<script src= "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</div>
+</body>
+</html>
